2c420a272a141f80bc06033c02385168516669d4
fidan <fidangasimova153@gmail.com>
2015-01-14 21:20:37 +0100

    added (very bad) error handling

--- colorfunction.js
+++ colorfunction.js
@@ -1,4 +1,14 @@
 function composeColor(r, g, b) {
-	return (r<<16) +(g<<8) + b
+    function ensureValid(x) {
+        if (x<0 || x>255) {
+            console.log("intensity must be between 0 and 255");
+        }
+    }
+    ensureValid(r);
+    ensureValid(g);
+    ensureValid(b);
+    var result = (r<<16) + (g<<8) + b;
+    return result;
 }
+console.log(composeColor(43,2300,54));
 
