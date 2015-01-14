b463c68f805cbd41e6d8892f568dd09e70dec5df
fidan <fidangasimova153@gmail.com>
2015-01-14 17:45:33 +0100

    Ilaria changed the name of her homework to one that resembels its content

--- extract_intensity3colors.js
+++
@@ -1,4 +0,0 @@
-function extractIntensity(r, g, b) {
-	return {colorValue};
-}
-console.log(extractIntensity(0xFFFF00));
---
+++ merge_color.js
@@ -0,0 +1,4 @@
+function mergeColor(r, g, b) {
+	return {colorValue};
+}
+console.log(mergeColor(0xFFFF00));
